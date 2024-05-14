23e790153cf459bec2261e4a24111dff5ebd4f8c
Matheus <mt121207@gmail.com>
2024-05-14 20:35:32 -0300

    Conteúdo da aula sobre Map, Ordenação.

---
+++ src/main/java/Map/Ordenacao/AgendaEventos.java
@@ -0,0 +1,52 @@
+package main.java.Map.Ordenacao;
+
+import jdk.jfr.Event;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEventos() {
+        this.eventoMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        eventoMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        LocalDate dataAtual = LocalDate.now();
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventoMap);
+
+        for(Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                System.out.println("O próximo evento: " + entry.getValue() + " acontecerá na data: " + entry.getKey());
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2022, Month.JULY, 15), "Evento 1", "Atração 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atração 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atração 3");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 5, 14), "Evento 4", "Atração 4");
+        agendaEventos.adicionarEvento(LocalDate.of(2024, 5, 15), "Evento 5", "Atração 5");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+
+    }
+
+}
