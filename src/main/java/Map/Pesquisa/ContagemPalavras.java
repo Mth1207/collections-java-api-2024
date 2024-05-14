9158005d763d32cc0b16e3c533a62f221cc3246b
Matheus <mt121207@gmail.com>
2024-05-14 19:43:07 -0300

    Exercício da aula sobre Map, Contagem de palavras.

---
+++ src/main/java/Map/Pesquisa/ContagemPalavras.java
@@ -0,0 +1,56 @@
+package main.java.Map.Pesquisa;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContagemPalavras {
+    private Map<String, Integer> contagemPalavrasMap;
+
+    public ContagemPalavras() {
+        this.contagemPalavrasMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, Integer contagem){
+        contagemPalavrasMap.put(palavra, contagem);
+    }
+
+    public void removerPalavra(String palavra){
+        if(!contagemPalavrasMap.isEmpty()){
+            contagemPalavrasMap.remove(palavra);
+        }
+    }
+
+    public int exibirContagem(){
+        int contagemTotal = 0;
+        for(int contagem : contagemPalavrasMap.values()){
+            contagemTotal += contagem;
+        }
+        return contagemTotal;
+    }
+
+    public String encontrarPalavrasMaisFrequente() {
+        String linguagemMaisFrequente = null;
+        int maiorContagem = 0;
+        for (Map.Entry<String, Integer> entry : contagemPalavrasMap.entrySet()) {
+            if (entry.getValue() > maiorContagem) {
+                maiorContagem = entry.getValue();
+                linguagemMaisFrequente = entry.getKey();
+            }
+        }
+        return linguagemMaisFrequente;
+    }
+
+    public static void main(String[] args) {
+        ContagemPalavras contagemLinguagens = new ContagemPalavras();
+
+        contagemLinguagens.adicionarPalavra("Java", 2);
+        contagemLinguagens.adicionarPalavra("Python", 8);
+        contagemLinguagens.adicionarPalavra("JavaScript", 1);
+        contagemLinguagens.adicionarPalavra("C#", 6);
+
+        System.out.println("Existem " + contagemLinguagens.exibirContagem() + " palavras.");
+
+        String linguagemMaisFrequente = contagemLinguagens.encontrarPalavrasMaisFrequente();
+        System.out.println("A linguagem mais frequente é: " + linguagemMaisFrequente);
+    }
+}
