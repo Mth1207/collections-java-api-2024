cab3efd728f2ae459f83e823d5994bd7ccc771e5
Matheus <mt121207@gmail.com>
2024-05-14 18:07:39 -0300

    Exercício sobre Ordenação utilizando Set

---
+++ src/main/java/set/Ordenacao/GerenciadorAlunos.java
@@ -0,0 +1,75 @@
+package main.java.set.Ordenacao;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class GerenciadorAlunos {
+    private Set<Aluno> alunoSet;
+
+    public GerenciadorAlunos() {
+        this.alunoSet = new HashSet<>();
+    }
+
+    public void adicionarAluno(String nome, Long matricula, double media) {
+        alunoSet.add(new Aluno(nome, matricula, media));
+    }
+
+    public void removerAluno(long matricula) {
+        Aluno alunoRemover = null;
+        if (!alunoSet.isEmpty()) {
+            for (Aluno a : alunoSet) {
+                if (a.getMatricula() == matricula) {
+                    alunoRemover = a;
+                    break;
+                }
+            }
+            alunoSet.remove(alunoRemover);
+        } else {
+            throw new RuntimeException("O conjunto está vazio!");
+        }
+        if (alunoRemover == null) {
+            System.out.println("Matricula não encontrada.");
+        }
+    }
+
+    public void exibirAlunosPorNome() {
+        Set<Aluno> alunoPorNome = new TreeSet<>(alunoSet);
+        if (!alunoSet.isEmpty()) {
+            System.out.println(alunoPorNome);
+        } else {
+            System.out.println("O conjunto está vazio!");
+        }
+    }
+
+    public void exibirAlunosPorNota() {
+        Set<Aluno> alunoPorNota = new TreeSet<>(new ComparatorNota());
+        if (!alunoSet.isEmpty()) {
+            alunoPorNota.addAll(alunoSet);
+            System.out.println(alunoPorNota);
+        } else {
+            System.out.println("O conjunto está vazio!");
+        }
+    }
+
+    public static void main(String[] args) {
+        GerenciadorAlunos gerenciadorAlunos = new GerenciadorAlunos();
+
+        gerenciadorAlunos.adicionarAluno("João", 123456L, 7.5);
+        gerenciadorAlunos.adicionarAluno("Maria", 123457L, 9.0);
+        gerenciadorAlunos.adicionarAluno("Carlos", 123458L, 5.0);
+        gerenciadorAlunos.adicionarAluno("Ana", 123459L, 6.8);
+
+        System.out.println("Alunos no gerenciador:");
+        System.out.println(gerenciadorAlunos.alunoSet);
+
+        gerenciadorAlunos.removerAluno(000L);
+        gerenciadorAlunos.removerAluno(123457L);
+        System.out.println(gerenciadorAlunos.alunoSet);
+
+        gerenciadorAlunos.exibirAlunosPorNome();
+
+        gerenciadorAlunos.exibirAlunosPorNota();
+
+    }
+}
